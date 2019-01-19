9ff1b16785c797b576cb25bc084b40510e282d29
visualstudio <visualstudio@122-PC17>
2019-01-18 19:18:07 -0500

    third commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,10 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(1 + 1);
+            Console.WriteLine(1 + 3);
+            int i = 1;
+        
         }
     }
 }
